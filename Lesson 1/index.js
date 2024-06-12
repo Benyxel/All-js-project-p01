d041104545a52b1fdeb3b00240fcbc761935efc1
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2024-06-12 01:11:25 +0000

    new

--- Lesson 1/index.js
+++ Lesson 1/index.js
@@ -8,16 +8,13 @@
  console.log(count)
 
  
- let bonusPoints = 50
- console.log(bonusPoints)
-
- bonusPoints = bonusPoints + 50
- console.log(bonusPoints)
-
- bonusPoints = bonusPoints - 75
- console.log(bonusPoints)
-
- bonusPoints = bonusPoints + 45
- console.log(bonusPoints)
-
+function increment() {
+  console.log("clicked")
+}
  
+function bernard() {
+ console.log(45)
+}
+
+bernard()
+
