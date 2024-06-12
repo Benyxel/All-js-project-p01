2f2d6c5fcc7b9c7be92c62ee7d628d2010bbaa88
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2024-06-12 00:32:30 +0000

    NEW

--- Lesson 1/index.js
+++ Lesson 1/index.js
@@ -2,17 +2,8 @@
 
 
 
-// let count = 5
+ let count = 5
 
-// console.log(count)
+ console.log(count)
 
-// // let myAge = 26
 
-// // console.log(myAge)
-
-let myAge = 26
-let humanDogRatio = 7
-
-let myDogAge = myAge * humanDogRatio
-
-console.log(myDogAge)
