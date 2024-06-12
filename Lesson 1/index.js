d672c7b4732ba5b65ef1076b638c9af77b856ab5
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2024-06-12 00:28:13 +0000

    NEW

--- Lesson 1/index.js
+++ Lesson 1/index.js
@@ -1,5 +1,18 @@
 // document.getElementById("count-el").innerText = 5
 
-let count = 0
 
-console.log(count)
+
+// let count = 5
+
+// console.log(count)
+
+// // let myAge = 26
+
+// // console.log(myAge)
+
+let myAge = 26
+let humanDogRatio = 7
+
+let myDogAge = myAge * humanDogRatio
+
+console.log(myDogAge)
