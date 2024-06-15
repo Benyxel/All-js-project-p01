20ef63b2b375f4ba744101c3b70dbd7ce5e65b4c
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2024-06-15 22:51:51 +0000

    new

---
+++ Lesson 1/Lessonn 2/js.js
@@ -0,0 +1,22 @@
+// let firtCard = 7
+// let secondCard = 15
+// let sum = firtCard + secondCard
+
+// if (sum < 21){
+//   console.log("Do you want to draw more card? 😊")
+// }
+// else if (sum === 21) {
+//   console.log("Woow You've got Blackjack! 😲")
+// }
+// else {
+//   console.log("You are out of the game! 😞")
+// }
+
+let age = 20
+
+if (age < 21){
+  console.log("You can not enter the club!")
+}
+else {
+  console.log("Welcome")
+}
