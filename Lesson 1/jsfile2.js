bb35f5d57cd290871e21f37fa519d90d5c6a776b
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2024-06-15 12:28:09 +0000

    new

--- Lesson 1/jsfile2.js
+++ Lesson 1/jsfile2.js
@@ -1,5 +1,21 @@
-let errorP = document.getElementById("error")
+// let errorParagraph = document.getElementById("error")
+// console.log("")
 
-function buy(){
-errorP.textContent = "Something went wrong"
+
+// function buy(){
+//   console.log("click")
+//   errorParagraph.textContent = "oops sorry something went wrong" 
+// }
+
+
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+
+function add(){
+ let add = num1 += 1
+ console.log(add)
 }
+
+
