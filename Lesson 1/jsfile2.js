49db942b03d2fb69ba268681f639381270de5bf3
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2024-06-15 12:28:09 +0000

    new

--- Lesson 1/jsfile2.js
+++ Lesson 1/jsfile2.js
@@ -1,5 +1,39 @@
-let errorP = document.getElementById("error")
+// let errorParagraph = document.getElementById("error")
+// console.log("")
 
-function buy(){
-errorP.textContent = "Something went wrong"
+
+// function buy(){
+//   console.log("click")
+//   errorParagraph.textContent = "oops sorry something went wrong" 
+// }
+
+
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+let sumA = document.getElementById("sum-el")
+
+
+function add() {
+  let sum = num1 += num2;
+  sumA.textContent = "Sum: " + sum;
 }
+
+
+function subtract(){
+  let sum = num1 -= num2
+  sumA.textContent = "Sum:" + sum
+}
+
+function divide(){
+  let sum = num1 /= num2
+  sumA.textContent = "Sum:" + sum
+}
+
+function multiply(){
+  let sum = num1 *= num2
+  sumA.textContent = "Sum:" + sum
+}
+
+
