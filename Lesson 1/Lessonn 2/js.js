53691f1de1c6f67ef0fe4d7f213c3b82602e7d49
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2024-06-15 23:14:13 +0000

    new

--- Lesson 1/Lessonn 2/js.js
+++ Lesson 1/Lessonn 2/js.js
@@ -20,16 +20,30 @@
 // else {
 //   console.log("Welcome")
 
-let age = 101
+// let age = 101
 
-if (age < 100) {
-  console.log("Not elegible")
+// if (age < 100) {
+//   console.log("Not elegible")
+// }
+
+// else if (age === 100) {
+//   console.log("Here is your birthday card from the king!")
+// }
+
+// else {
+//   console.log("Not elegible, you have already gotten a card")
+
+
+let firstCard = 10
+let secondCard = 12
+let sum = firstCard + secondCard
+
+if (sum <= 20){
+  console.log("Do you want to draw a new card?")
 }
-
-else if (age === 100) {
-  console.log("Here is your birthday card from the king!")
+else if (sum === 21){
+  console.log("You have got a Blackjack!")
 }
-
 else {
-  console.log("Not elegible, you have already gotten a card")
+  console.log(" You are out the game!")
 }
