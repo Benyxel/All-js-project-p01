f071f676ead1b534753f77979a31b975b67936ee
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2024-07-04 21:42:42 +0000

    new

--- Lesson 3/java.js
+++ Lesson 3/java.js
@@ -1,2 +1,16 @@
- 
+ let password
 
+ if (password == 8){
+  console.log("Welcom");
+ }
+else if (password <= 8){
+  console.log("Password is too short");
+}
+
+else if (password >= 8){
+  console.log("Too Long" + " Password should be 8 characters");
+}
+
+else {
+console.log("Please provide a password");
+}
