f84165d105fa4f71c57e19e0771f2899206c57fb
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2024-07-05 22:50:02 +0000

    new

--- Lesson 3/java.js
+++ Lesson 3/java.js
@@ -13,8 +13,20 @@
 //  console.log("Bernard",i++); 
 // }
 
-let i = 20
+// let i = 20
 
-do{
-  console.log("Bernard",i); i++
-} while (i <= 400); 
+// do{
+//   console.log("Bernard",i); i++
+// } while (i <= 400); 
+
+const favSingers = ["Sarkodie", "King Paluta", "Patapaa"];
+console.log(favSingers[0]);
+
+const favNumbers = [ 12, 30, 19, 2]
+console.log(favNumbers);
+
+const mixedArr = ["string",["otherarray"], "123", "true"];
+console.log(mixedArr[0]);
+console.log(mixedArr[1][0]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
