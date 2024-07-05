aa547ae745d89125c4ee00db68dd9ab38b5e433d
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2024-07-05 22:15:09 +0000

    new

--- Lesson 3/java.js
+++ Lesson 3/java.js
@@ -1,7 +1,20 @@
 
   
 
-for (let i = 1; i <= 1000; i++) {
-  console.log("Bernard", i);
+// for (let i = 1; i <= 1000; i++) {
+//   console.log("Bernard", i);
   
-}
+// }
+
+
+// let i = 1
+
+// while (i <= 100){
+//  console.log("Bernard",i++); 
+// }
+
+let i = 20
+
+do{
+  console.log("Bernard",i); i++
+} while (i <= 400); 
