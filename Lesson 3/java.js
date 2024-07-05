882d784eeaa4a6e423b24598662bfc85e0f5588f
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2024-07-05 22:53:28 +0000

    new

--- Lesson 3/java.js
+++ Lesson 3/java.js
@@ -27,6 +27,6 @@ console.log(favNumbers);
 
 const mixedArr = ["string",["otherarray"], "123", "true"];
 console.log(mixedArr[0]);
-console.log(mixedArr[1][0]);
+console.log(mixedArr[1]);
 console.log(mixedArr[2]);
 console.log(mixedArr[3]);
