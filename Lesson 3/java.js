8fbcb57135bcc80c2b41a1e165334cf9d3012f1f
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2024-07-06 22:04:08 +0000

    new

--- Lesson 3/java.js
+++ Lesson 3/java.js
@@ -19,14 +19,19 @@
 //   console.log("Bernard",i); i++
 // } while (i <= 400); 
 
-const favSingers = ["Sarkodie", "King Paluta", "Patapaa"];
-console.log(favSingers[0]);
+// const favSingers = ["Sarkodie", "King Paluta", "Patapaa"];
+// console.log(favSingers[0]);
 
-const favNumbers = [ 12, 30, 19, 2]
-console.log(favNumbers);
+// const favNumbers = [ 12, 30, 19, 2]
+// console.log(favNumbers);
 
-const mixedArr = ["string",["otherarray"], "123", "true"];
-console.log(mixedArr[0]);
-console.log(mixedArr[1]);
-console.log(mixedArr[2]);
-console.log(mixedArr[3]);
+// const mixedArr = ["string",["otherarray"], "123", "true"];
+// console.log(mixedArr[0]);
+// console.log(mixedArr[1]);
+// console.log(mixedArr[2]);
+// console.log(mixedArr[3]);
+
+// const name = ["bernard","yeboah", "Boanu"]
+
+// console.log(name.slice(0, 2));
+
